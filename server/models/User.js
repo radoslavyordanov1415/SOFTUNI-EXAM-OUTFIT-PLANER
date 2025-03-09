4cd968495b78e2f572b978a3bcfc785ec8631ff3
radoslavyordanov1415 <radoslavyordanov1415@gmail.com>
2025-03-09 14:06:52 +0100

    Create User schema with fields for registration and hashing the password

---
+++ server/models/User.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: [true, 'Username is required'],
+        unique: true
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required'],
+    }
+
+
+});
+
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (err) {
+        next(err);
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
