ef8f9043d3d1bb2febe8c6ba8f5621a39bd8f1c7
radoslavyordanov1415 <radoslavyordanov1415@gmail.com>
2025-03-09 14:17:07 +0100

    Implement user registration route with JWT token generation and cookie storage

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,49 @@
+import express from 'express';
+import jwt from 'jsonwebtoken';
+import User from '../models/User.js';
+
+const router = express.Router();
+
+router.get('/register', async (req, res) => {
+    const {
+        username,
+        email,
+        password
+    } = req.body;
+
+    try {
+        // If user exists
+        const existingUser = await User.findOne({ email })
+        if (existingUser) {
+            return res.status(400).json({ message: 'User already exists.' });
+        };
+
+        // Creating new user
+        const newUser = new User({
+            username,
+            email,
+            password
+        });
+
+        await newUser.save();
+
+        const token = jwt.sign(
+            { userId: newUser._id, username: newUser.username },
+            process.env.JWT_SECRET,
+            { expiresIn: '1h' }
+        );
+
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: 'strict',
+            maxAge: 3600000,
+        });
+
+        res.status(201).json({ message: "User registered successfully" });
+
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    };
+
+});
