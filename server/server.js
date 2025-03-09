a2f456bc7f25f17fe0269a19e3c5751229132c2d
radoslavyordanov1415 <radoslavyordanov1415@gmail.com>
2025-03-09 14:00:39 +0100

    Set up Express server in server.js with basic configuration

---
+++ server/server.js
@@ -0,0 +1,35 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import cors from 'cors';
+import mongoose from "mongoose";
+
+const app = express();
+
+// Middleware configuration
+app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,POST,PUT,DELETE',
+    credentials: true,
+}));
+
+//TODO: Routes
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("MongoDB connected!");
+    } catch (error) {
+        console.error("Database connection failed:", error);
+        process.exit(1);
+    }
+};
+
+// Start the server
+const PORT = process.env.PORT || 5002; // Ensure this matches your .env
+app.listen(PORT, async () => {
+    await connectDB();
+    console.log(`Server running on port ${PORT}`);
+});
